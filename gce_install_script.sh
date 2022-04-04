9950731cea2b791bd179ee8494290a47bccf435e
Teerapat12 <teerapat.time12@gmail.com>
2022-04-04 13:52:40 -0400

    add install script

---
+++ gce_install_script.sh
@@ -0,0 +1,16 @@
+# nvidia-driver https://cloud.google.com/compute/docs/gpus/install-drivers-gpu
+
+curl https://raw.githubusercontent.com/GoogleCloudPlatform/compute-gpu-installation/main/linux/install_gpu_driver.py --output install_gpu_driver.py
+sudo python3 install_gpu_driver.py
+
+# miniconda 
+sudo apt-get update
+sudo apt-get install bzip2 libxml2-dev wget
+
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
+bash Miniconda3-latest-Linux-x86_64.sh
+rm Miniconda3-latest-Linux-x86_64.sh
+source ~/.bashrc
+
+# Project setup
+# Continue from here
