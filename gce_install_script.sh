a3eb495c0d0500941d9d1be7d6b5d785cc0a2409
Teerapat12 <teerapat.time12@gmail.com>
2022-04-04 15:07:15 -0400

    update install script

--- gce_install_script.sh
+++ gce_install_script.sh
@@ -1,20 +1,25 @@
-# nvidia-driver https://cloud.google.com/compute/docs/gpus/install-drivers-gpu
+set -e # Stop the script if something fails
 
+
+echo "Installing nvidia-driver"
+# nvidia-driver https://cloud.google.com/compute/docs/gpus/install-drivers-gpu
 curl https://raw.githubusercontent.com/GoogleCloudPlatform/compute-gpu-installation/main/linux/install_gpu_driver.py --output install_gpu_driver.py
 sudo python3 install_gpu_driver.py
 
+echo "Installilng miniconda"
 # miniconda 
 sudo apt-get update
 sudo apt-get install bzip2 libxml2-dev wget
 
 wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
-bash Miniconda3-latest-Linux-x86_64.sh
+yes | bash Miniconda3-latest-Linux-x86_64.sh
 rm Miniconda3-latest-Linux-x86_64.sh
 source ~/.bashrc
 
-# Project setup
+
+echo "Setting up project"
 # Continue from here
 make envload
-miniconda activate python39
-conda install pytorch torchvision torchaudio cudatoolkit=11.3 -c pytorch
+conda activate python39
+# conda install pytorch torchvision torchaudio cudatoolkit=11.3 -c pytorch
 
