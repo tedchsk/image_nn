652038b1e94c0ea9da0814e46052e8a79047ab3d
Teerapat12 <teerapat.time12@gmail.com>
2022-04-09 08:24:15 -0400

    update report.py

--- scripts/report.py
+++ scripts/report.py
@@ -11,7 +11,13 @@ df = combine_run_experiments(os.path.join(RESULT_DIR, latest_dir))
 print(df.shape)
 print(df.head(100))
 
-summarized = df.groupby("exp_name").agg({"train_acc": ["mean", "std"], "val_acc": [
-    "mean", "std"], "test_acc": ["mean", "std"]})
+summarized = df.groupby("exp_name").agg(
+        {
+            "train_acc": ["mean", "std"],
+            "val_acc": ["mean", "std"],
+            "test_acc": ["mean", "std"],
+            "n_epochs": ["max", "count"]
+            }
+        )
 
 print(summarized.head(100))
