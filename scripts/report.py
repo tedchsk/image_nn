e1d53f57de08884712538f193422cd958c939152
Teerapat12 <teerapat.time12@gmail.com>
2022-04-09 08:26:41 -0400

    update report.py

--- scripts/report.py
+++ scripts/report.py
@@ -8,15 +8,16 @@ dirs = os.listdir(RESULT_DIR)
 latest_dir = sorted(dirs)[-1]
 df = combine_run_experiments(os.path.join(RESULT_DIR, latest_dir))
 
-print(df.shape)
-print(df.head(100))
+for col in ["train", "test", "val"]:
+    df[f"{col}_err"] = 1 - df[f"{col}_acc"]
 
 summarized = df.groupby("exp_name").agg(
         {
-            "train_acc": ["mean", "std"],
-            "val_acc": ["mean", "std"],
-            "test_acc": ["mean", "std"],
-            "n_epochs": ["max", "count"]
+            "train_er": ["mean", "std"],
+            "val_err": ["mean", "std"],
+            "test_err": ["mean", "std"],
+            "n_epochs": ["count"],
+            "train_time": ["mean", "std"],
             }
         )
 
