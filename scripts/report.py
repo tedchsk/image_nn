c3de8548eec74a1aaf7afee7e01934433ed0e8aa
Teerapat12 <teerapat.time12@gmail.com>
2022-04-09 08:21:34 -0400

    update

--- scripts/report.py
+++ scripts/report.py
@@ -9,6 +9,7 @@ latest_dir = sorted(dirs)[-1]
 df = combine_run_experiments(os.path.join(RESULT_DIR, latest_dir))
 
 print(df.shape)
+print(df.head(100))
 
 summarized = df.groupby("exp_name").agg({"train_acc": ["mean", "std"], "val_acc": [
     "mean", "std"], "test_acc": ["mean", "std"]})
