172ba87caf7206ef4c2f48eae20e710c843215bf
Teerapat12 <teerapat.time12@gmail.com>
2022-04-18 19:29:00 -0400

    remove model_summary for now

--- core/runner.py
+++ core/runner.py
@@ -12,7 +12,6 @@ from core.data_loader import data_loader_builder
 from core.logger.default import LoggerDefault
 from core.logger.logger_abc import LoggerABC
 from core.model.resnet import ResNet
-from torchsummary import summary
 
 SEED_FOR_FOLDS = [42, 124, 6124, 1235, 1265, 1734,
                   134, 16, 12, 61, 123, 643, 6143, 6413, 712, 1024, 612, 6124, 995, 12512, 16141236]
@@ -179,8 +178,7 @@ class Runner:
             test_info = {
                 'test_time': np.round(time.time()-start_time, 5),
                 'test_loss': np.round(running_loss / dataset_size, 5),
-                'test_acc': np.round(running_corrects / dataset_size, 5),
-                'model_summary': str(summary(model))
+                'test_acc': np.round(running_corrects / dataset_size, 5)
             }
 
             return test_info
